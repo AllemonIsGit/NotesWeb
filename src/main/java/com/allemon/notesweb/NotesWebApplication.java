f99e7a052c75be552a1fbe88168aff6043e9c7a9
Allemon <allemon3@gmail.com>
2022-07-22 20:38:01 +0200

    Shit has been cleaned

--- src/main/java/com/allemon/notesweb/NotesWebApplication.java
+++ src/main/java/com/allemon/notesweb/NotesWebApplication.java
@@ -2,8 +2,6 @@ package com.allemon.notesweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class NotesWebApplication {
--- src/main/java/com/allemon/notesweb/controllers/UserController.java
+++ src/main/java/com/allemon/notesweb/controllers/UserController.java
@@ -1,13 +1,10 @@
 package com.allemon.notesweb.controllers;
 
-import com.allemon.notesweb.domain.dto.CreateUserRequest;
 import com.allemon.notesweb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin
 @RestController
